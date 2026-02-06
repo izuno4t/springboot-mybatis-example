89c9fdfd4269160515febbafc9f839bb0130a719
izuno4t <9846+izuno4t@users.noreply.github.com>
2026-02-06 13:07:07 +0900

    remove unused Dockerfile, gradlew, and configuration files

--- src/main/resources/db/migration/V1__Create_city.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS city;
-CREATE TABLE city (
-    id serial primary key,
-    name varchar(200),
-    state varchar(2),
-    country varchar(2)
-);
--- init data
-insert into city (name, state, country) values ('San Francisco', 'CA', 'US');
--- src/main/resources/db/migration/V2__Create_todo.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS todo;
-CREATE TABLE todo (
-    id serial primary key,
-    title varchar(200) NOT NULL,
-    details TEXT,
-    finished char(1) NOT NULL DEFAULT '0'
-);
-
-INSERT INTO todo(title, details) VALUES('ミルクをかう','ミルクを2本買う')
